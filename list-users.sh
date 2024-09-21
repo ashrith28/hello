fc495d40199fb7f227f326bb98e8b18b26ceb076
ashrith28 <ashu.ashrith28@gmail.com>
2024-09-21 12:13:06 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -8,8 +8,8 @@ USERNAME=$username
 TOKEN=$token
 
 # User and Repository information
-REPO_OWNER=$1
-REPO_NAME=$2
+ashrith28=$1
+hello=$2
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
@@ -22,21 +22,21 @@ function github_api_get {
 
 # Function to list users with read access to the repository
 function list_users_with_read_access {
-    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+    local endpoint="repos/${ashrith28}/${hello}/collaborators"
 
     # Fetch the list of collaborators on the repository
     collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
 
     # Display the list of collaborators with read access
     if [[ -z "$collaborators" ]]; then
-        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+        echo "No users with read access found for ${ashrith28}/${hello}."
     else
-        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "Users with read access to ${ashrith28}/${hello}:"
         echo "$collaborators"
     fi
 }
 
 # Main script
 
-echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+echo "Listing users with read access to ${ashrith28}/${hello}..."
 list_users_with_read_access
